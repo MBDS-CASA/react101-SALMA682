9353b22c8741049a8662746e516e1836c60e5a4d
SalmaIghir <ighirouaioursalma@gmail.com>
2025-12-22 17:11:11 +0100

    TD03: Afficher les details de note

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import './App.css'
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
+import RandomNote from "./components/RandomNote.jsx";
 function App() {
 
   return (
     <>
       <Header />
       <MainContent />
+      <RandomNote />
       <Footer />
     </>
   )

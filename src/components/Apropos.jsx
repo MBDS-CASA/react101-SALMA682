813225efd3bbd440910ffa82daf9d59de5182f5b
SalmaIghir <ighirouaioursalma@gmail.com>
2025-12-23 09:36:01 +0100

    TD05: Remplir le contenu du composant A propos

---
+++ src/components/Apropos.jsx
@@ -0,0 +1,12 @@
+function APropos() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>À propos</h2>
+      <p>
+        Ce projet est réalisé par <strong>Salma Ighir</strong> – 2025
+      </p>
+    </div>
+  );
+}
+
+export default APropos;
